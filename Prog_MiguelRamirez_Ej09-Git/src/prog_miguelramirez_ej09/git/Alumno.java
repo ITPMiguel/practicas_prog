b3d4a92010aaf42feaf33d52ebccfd1924960cc0
Miguel <miguel.ramirez@ponienteformacion.com>
2020-02-04 13:54:11 +0100

    Clase Alum. a medias

--- Prog_MiguelRamirez_Ej09-Git/src/prog_miguelramirez_ej09/git/Alumno.java
+++ Prog_MiguelRamirez_Ej09-Git/src/prog_miguelramirez_ej09/git/Alumno.java
@@ -6,6 +6,7 @@
 package prog_miguelramirez_ej09.git;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -17,21 +18,75 @@ public class Alumno {
     private ArrayList notas;
     private static int n_asignaturas = 5;
 
+    /**
+     * constructor por defecto
+     */
     public Alumno() {
         nombre = "";
         notas = new ArrayList<Double>(n_asignaturas);
     }
 
+    /**
+     * constructor por parametros
+     *
+     * @param nombre nombre del alumno
+     */
     public Alumno(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * meotodo get dara devolver el nombre del alumno
+     *
+     * @return devuelve el nombre del alumno
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * metodo set para guardar el nombre del alumno
+     *
+     * @param nombre nombre del alumno
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    /**
+     * Pide notas por teclado, tantas como el numero de asignaturas
+     *
+     * @throws RangoException en caso de que estas no sean entre 0 - 10 salta
+     */
+    public void pedirNotas() throws RangoException {
+        Scanner teclado = new Scanner(System.in);
+        try {
+            for (int i = 0; i < n_asignaturas; i++) {
+                System.out.print("Dame la +" + (i + 1) + " nota del alumno +" + nombre + ":");
+                double nota = teclado.nextDouble();
+                if (nota < 0 || nota > 10) {
+                    throw new RangoException("Nota no valida.");
+                }
+                notas.add(nota);
+            }
+        } catch (RangoException e) {
+            System.out.println(" (Error: " + e.toString() + ")");
+        }
+    }
+
+    public void modificarNota(int posicion, double nnota) throws RangoException, MisteriosaException {
+
+        try {
+            if (nnota < 0 || nnota > 10) {
+                throw new RangoException("Nota no valida.");
+            }
+            if (posicion > n_asignaturas || posicion < 0) {
+                throw new MisteriosaException (0,0);
+            }
+            notas.add(nnota);
+        } catch (RangoException e) {
+            System.out.println(" (Error: " + e.toString() + ")");
+        }
     }
 
 }
